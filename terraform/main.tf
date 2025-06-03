a929e02308602c087b4bda4da1359a214551ba72
MrNovan <toni.justdoit@bk.ru>
2025-06-03 18:39:07 +0300

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -139,52 +139,47 @@ resource "yandex_alb_listener" "app-listener" {
 }
 
 # --- База данных PostgreSQL ---
-resource "yandex_mdb_mysql_cluster" "dbcluster" {
-  name        = "dbcluster-mollyj"
+resource "yandex_mdb_postgresql_cluster" "dbcluster" {
+  name        = "tfhexlet"
   environment = "PRESTABLE"
   network_id  = yandex_vpc_network.net.id
-  version     = "8.0"
 
-  resources {
-    resource_preset_id = "s2.micro"
-    disk_type_id       = "network-ssd"
-    disk_size          = 16
+  config {
+    version = var.yc_postgresql_version
+    resources {
+      resource_preset_id = "s2.micro"
+      disk_type_id       = "network-ssd"
+      disk_size          = 15
+    }
+    postgresql_config = {
+      max_connections    = 100
+    }
   }
 
-  mysql_config = {
-    sql_mode                      = "ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION"
-    max_connections               = 100
-    default_authentication_plugin = "MYSQL_NATIVE_PASSWORD"
-    innodb_print_all_deadlocks    = true
+  maintenance_window {
+    type = "WEEKLY"
+    day  = "SAT"
+    hour = 12
   }
 
   host {
-    zone      = var.zone_id
+    zone      = "ru-central1-a"
     subnet_id = yandex_vpc_subnet.subnet.id
   }
-
-  depends_on = [yandex_vpc_network.net, yandex_vpc_subnet.subnet]
 }
 
-resource "yandex_mdb_mysql_user" "dbuser" {
-  cluster_id = yandex_mdb_mysql_cluster.dbcluster.id
+resource "yandex_mdb_postgresql_user" "dbuser" {
+  cluster_id = yandex_mdb_postgresql_cluster.dbcluster.id
   name       = var.db_user
   password   = var.db_password
-
-  permission {
-    database_name = yandex_mdb_mysql_database.db.name
-    roles         = ["ALL"]
-  }
-
-  depends_on = [yandex_mdb_mysql_cluster.dbcluster]
+  depends_on = [yandex_mdb_postgresql_cluster.dbcluster]
 }
 
-resource "yandex_mdb_mysql_database" "db" {
-  cluster_id = yandex_mdb_mysql_cluster.dbcluster.id
+resource "yandex_mdb_postgresql_database" "db" {
+  cluster_id = yandex_mdb_postgresql_cluster.dbcluster.id
   name       = var.db_name
-
-  depends_on = [yandex_mdb_mysql_cluster.dbcluster]
-  lifecycle {
-    ignore_changes = [name]
-  }
+  owner      = yandex_mdb_postgresql_user.dbuser.name
+  lc_collate = "en_US.UTF-8"
+  lc_type    = "en_US.UTF-8"
+  depends_on = [yandex_mdb_postgresql_cluster.dbcluster]
 }
