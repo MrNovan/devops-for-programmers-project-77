4e34fcc33accfe2fb19991827681a02ee0d42bcd
MrNovan <toni.justdoit@bk.ru>
2025-06-03 16:37:17 +0300

    fix mysql

--- terraform/main.tf
+++ terraform/main.tf
@@ -139,39 +139,52 @@ resource "yandex_alb_listener" "app-listener" {
 }
 
 # --- База данных PostgreSQL ---
-resource "yandex_mdb_postgresql_cluster" "app-db" {
-  name        = "app-db"
+resource "yandex_mdb_mysql_cluster" "dbcluster" {
+  name        = "dbcluster-mollyj"
   environment = "PRESTABLE"
-  network_id  = yandex_vpc_network.app-network.id
+  network_id  = yandex_vpc_network.net.id
+  version     = "8.0"
 
-  config {
-    version = "14"
+  resources {
+    resource_preset_id = "s2.micro"
+    disk_type_id       = "network-ssd"
+    disk_size          = 16
+  }
 
-    resources {
-      resource_preset_id = "s2.micro"
-      disk_size          = 10
-      disk_type_id       = "network-hdd"
-    }
+  mysql_config = {
+    sql_mode                      = "ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION"
+    max_connections               = 100
+    default_authentication_plugin = "MYSQL_NATIVE_PASSWORD"
+    innodb_print_all_deadlocks    = true
   }
 
   host {
-    zone      = "ru-central1-a"
-    subnet_id = yandex_vpc_subnet.app-subnet.id
+    zone      = var.zone_id
+    subnet_id = yandex_vpc_subnet.subnet.id
   }
 
-  database {
-    name  = "app_db"
-    owner = "pg-user"
-  }
+  depends_on = [yandex_vpc_network.net, yandex_vpc_subnet.subnet]
 }
 
-resource "yandex_mdb_postgresql_user" "pg-user" {
-  name     = "pg-user"
-  cluster_id = yandex_mdb_postgresql_cluster.app-db.id
-  password = "your-secret-password"
+resource "yandex_mdb_mysql_user" "dbuser" {
+  cluster_id = yandex_mdb_mysql_cluster.dbcluster.id
+  name       = var.db_user
+  password   = var.db_password
 
   permission {
-    database_name = "app_db"
-    roles         = ["OWNER", "CONNECT"]
+    database_name = yandex_mdb_mysql_database.db.name
+    roles         = ["ALL"]
+  }
+
+  depends_on = [yandex_mdb_mysql_cluster.dbcluster]
+}
+
+resource "yandex_mdb_mysql_database" "db" {
+  cluster_id = yandex_mdb_mysql_cluster.dbcluster.id
+  name       = var.db_name
+
+  depends_on = [yandex_mdb_mysql_cluster.dbcluster]
+  lifecycle {
+    ignore_changes = [name]
   }
 }
