b35805cb864c4cb39c5440cec289c11bf0cc2b0e
MrNovan <toni.justdoit@bk.ru>
2025-06-04 11:11:00 +0300

    fix

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,8 @@
 # terraform/outputs.tf
 output "lb_ip" {
-  value = yandex_lb_listener.app-listener.external_address_spec[0].address
+  value = yandex_lb_listener.test-balancer.external_address_spec[0].address
 }
 
 output "db_host" {
-  value = yandex_mdb_postgresql_cluster.app-db.host[0].name
+  value = yandex_mdb_postgresql_cluster.dbcluster.host[0].name
 }
