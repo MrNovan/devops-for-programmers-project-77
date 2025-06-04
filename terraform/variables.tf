2f7617a04b81b6c7ff4a98d23e95d52468180902
MrNovan <toni.justdoit@bk.ru>
2025-06-04 10:41:45 +0300

    add var

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,3 +14,46 @@ variable "folder_id" {
 variable "ssh_pub" {
   type    = string
 }
+
+variable "db_user" {
+  description = "DB user"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "DB password"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_database" {
+  description = "DB database"
+  type        = string
+}
+
+variable "yc_postgresql_version" {
+  description = "DB yc postgresql version"
+  type        = string
+}
+
+variable "domen" {
+  description = "Domen name"
+  type        = string
+}
+
+variable "network_name" {
+  description = "Network name"
+  type        = string
+}
+
+variable "subnet_name" {
+  description = "Subnet name"
+  type        = string
+}
+
+variable "path_to_file" {
+  description = "Path to ssh file"
+  type        = string
+  sensitive   = true
+}
