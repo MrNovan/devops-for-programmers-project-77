fb482e98e784a370711c59ae1554c44294df6ec3
MrNovan <toni.justdoit@bk.ru>
2025-06-04 09:44:36 +0300

    fix

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,7 +8,8 @@ terraform {
 }
 
 provider "yandex" {
-  token     = var.yc_token
+  yc_token = var.yc_token
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
+  zone      = "ru-central1-a"
 }
